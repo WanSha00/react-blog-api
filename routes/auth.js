7350e45ce6eb24d11391d97037819e3b40ef309f
WanSha00 <88984549+WanSha00@users.noreply.github.com>
2024-04-15 19:51:00 +0800

    add login routing

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 //register
-router.post("/register", async(req,res) => {
-    try {
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(req.body.password, salt);
 
-        const salt = await bcrypt.genSalt(saltRounds);
-        const hash = await bcrypt.hash(req.body.password, salt);
-        
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hash
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
 
-        });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-        const user = await newUser.save();
-        res.status(200).json(user);
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+    });
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
+    if (!user) {
+      return res.status(400).json("Wrong credentials!");
     }
-})
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+
+    if (!validated) {
+      return res.status(400).json("Wrong credentials!");
+    }
+
+    //show all details except password
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
