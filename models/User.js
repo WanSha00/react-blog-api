772f88303d10ad8395e0983062a79edbf32e1f0f
WanSha00 <88984549+WanSha00@users.noreply.github.com>
2024-04-21 17:51:41 +0800

    add user bio field

--- models/User.js
+++ models/User.js
@@ -20,6 +20,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    bio: {
+      type: String,
+      default: "",
+    },
   },
   { timestamps: true }
 );
