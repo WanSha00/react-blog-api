6c95b53e6e7eb95c9701e6198a067c22f842a25c
WanSha00 <88984549+WanSha00@users.noreply.github.com>
2024-05-02 20:08:45 +0800

    added upload error checking

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/database");
 const cors = require("cors");
 const multer = require("multer");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
@@ -17,7 +17,7 @@ const categoryRoutes = require("./routes/categories");
 dotenv.config();
 app.use(cors());
 app.use(express.static("public"));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //db connection
@@ -50,26 +50,29 @@ function parsePublicId(cloudinaryUrl) {
   }
 }
 
-const upload = multer({ storage: storage });
+try {
+  const upload = multer({ storage: storage });
+  // Handle file upload
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+      const publicId = parsePublicId(req.file.path);
+      res.json({ url: req.file.path, id: publicId });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  });
+} catch (error) {
+  console.log(error);
+  res.status(500).json(error);
+}
 
-// Handle file upload
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    const publicId = parsePublicId(req.file.path);
-  res.json({ url: req.file.path, id: publicId });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-    
-  }
-  
-});
 //----------------------
 
 //routes
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
   res.send("api connected");
-})
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
